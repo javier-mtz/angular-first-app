11ecd54a68270e3efe93ef71b14573294066eb93
Jonathan Cobos <jjcd1706@gmail.com>
2024-02-15 10:52:08 -0600

    Nuevos componentes
    Se crean los componentes correspondientes de cada sección del sitio web

---
+++ src/app/Components/info-car/info-car.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-car',
+  standalone: true,
+  imports: [],
+  templateUrl: './info-car.component.html',
+  styleUrl: './info-car.component.css'
+})
+export class InfoCarComponent {
+
+}
