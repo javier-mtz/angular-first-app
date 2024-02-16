133d472a571eb011ffe63a2990fbee11589a6a78
Jonathan Cobos <jjcd1706@gmail.com>
2024-02-16 14:22:20 -0600

    ConfiguracionGrids
    Se añaden grids de las marcas de autos

--- src/app/Components/brands/brands.component.ts
+++ src/app/Components/brands/brands.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-brands',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule],
   templateUrl: './brands.component.html',
   styleUrl: './brands.component.css'
 })
