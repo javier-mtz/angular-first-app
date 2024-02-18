3c7a5cf298d61ed1a8d99c36e2c70e7783341627
JorgeAlexis1 <109886965+JorgeAlexis1@users.noreply.github.com>
2024-02-17 23:55:30 -0600

    Update profile.component.ts

--- src/app/Components/profile/profile.component.ts
+++ src/app/Components/profile/profile.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common'; // Asegúrate de importar CommonModule
+
+
 
 @Component({
   selector: 'app-profile',
-  standalone: true,
-  imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrls: ['./profile.component.css'],
+  standalone: true,
+  imports: [RouterOutlet, MatSlideToggleModule, MatGridListModule, MatCardModule, MatToolbarModule, CommonModule],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  brand: string = ''; // Inicializando la propiedad brand
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.brand = params['brand'];
+      // Puedes acceder a this.brand aquí una vez que tenga un valor asignado
+      if (this.brand) {
+        // Hacer algo con this.brand
+      }
+    });
+  }
 }
