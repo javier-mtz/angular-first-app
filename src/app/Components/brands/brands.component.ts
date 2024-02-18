fc7dbcc8e41512c11e7690bd107e860056f1cb68
JorgeAlexis1 <109886965+JorgeAlexis1@users.noreply.github.com>
2024-02-17 23:53:40 -0600

    Update brands.component.ts

--- src/app/Components/brands/brands.component.ts
+++ src/app/Components/brands/brands.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { RouterModule } from '@angular/router'; // Asegúrate de importar RouterModule
+
 
 @Component({
   selector: 'app-brands',
   standalone: true,
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, RouterModule],
   templateUrl: './brands.component.html',
   styleUrl: './brands.component.css'
 })
