a0c56847d55bb1f5af627febbdb0ef0e7beaa856
Jonathan Cobos <jjcd1706@gmail.com>
2024-02-18 02:31:28 -0600

    ConfiguracionGrids_v3
    Acomodo de imágenes de cada grid, cambio de banner de la sección grid, se agregó una descripción(aún hay dudas de si dejarlo o quitarlo), se agregó el fondo de la carretera en la sección grid

--- src/app/Components/brands/brands.component.ts
+++ src/app/Components/brands/brands.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { RouterModule } from '@angular/router'; // Asegúrate de importar RouterModule
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-brands',
   standalone: true,
-  imports: [MatGridListModule, RouterModule],
+  imports: [MatGridListModule, RouterModule, MatSlideToggleModule, MatCardModule, MatToolbarModule],
   templateUrl: './brands.component.html',
   styleUrl: './brands.component.css'
 })
