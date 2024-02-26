2cf8a13da237a078b11bace3fb4a8bb9869334cc
Jonathan Cobos <jjcd1706@gmail.com>
2024-02-25 20:06:39 -0600

    Componente Header
    Se crea el componente Header y se remplaza en todos los componentes

---
+++ src/app/Components/header/header.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ActivatedRoute } from '@angular/router';
+import { data } from '../../../assets/data/info';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbarModule, CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent implements OnInit{
+  @Input() componentName: string | undefined;
+  @Input() currentComponent: string | undefined;
+  marca: string = '';
+  color: string = '';
+  logo: string = '';
+
+  filteredData: any[] | undefined = []; 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.marca = params['brand'];
+      if (this.marca) {
+        this.filteredData = data.filter((item) => item.marca === this.marca);
+        if (this.filteredData.length > 0) {
+          this.color = this.filteredData[0].paletaColores[0] || ''; // Asigna el color o un valor por defecto
+          this.logo = this.filteredData[0].logo || ''; // Asigna el logo o un valor por defecto
+        }
+      }
+    });
+  }
+}
