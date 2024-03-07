9678222d0b01181655adbb78fa4c4040f08a11e4
javier-mtz <jmartinez@interfazsoft.com>
2024-03-07 14:00:54 -0600

    Se agrega funcionalidad en header para clickear el titulo e ir a home, asi como la imagen cuando no aparece el boton de usuario, por fin se agrega la imagen de fondo en home

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HeaderComponent } from "../header/header.component";
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
