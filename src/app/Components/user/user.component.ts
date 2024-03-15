07e1385b02b0ed3a7f5ee86a806e6a976dbf0797
Jonathan Cobos <159668235+JJCD17@users.noreply.github.com>
2024-03-15 01:28:48 -0600

    Diseño de Perfil de usuario

--- src/app/Components/user/user.component.ts
+++ src/app/Components/user/user.component.ts
@@ -3,9 +3,11 @@ import { HeaderComponent } from '../header/header.component';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 import { CommonModule } from '@angular/common';
 
@@ -19,13 +21,24 @@ import { CommonModule } from '@angular/common';
     MatFormFieldModule,
     MatIconModule,
     CommonModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  hide = true;
 
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
   constructor() { }
 
   ngOnInit(): void {
